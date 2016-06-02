c67c9b4164b0e2614dec8dd79664689026fbb518
jeremy.coutellec <jeremy.coutellec@gmail.com>
2016-06-02 16:39:10 +0200

    new2

--- app.js
+++ app.js
@@ -4,19 +4,30 @@
     var app = angular.module('bookStore', []);
 
     app.controller('StoreController',  function (){
-        this.products = book;
+        this.products = books;
         this.activeFilter = 'name';
+        this.reverse = false;
 
-        this.filterOn = function(param){
-            this.activeFilter = param;
+        this.isSelect = function(param){
+            return this.tab === param;
         }
 
+        this.selTab = function(param){
+            this.tab = param;
+        }
+        this.filterOn = function(param){
+            if(this.activeFilter != param){
+                this.activeFilter = param;
+                this.reverse = false;
+            }else
+                this.reverse = true;
+        }
     });
 
     var books = [
         {
             name: 'Tome 1 : La Première Leçon du Sorcier',
-            price: 25,
+            price: 8,
             image: "images/1_wizardsfirstrule.jpg",
             reviews: [{
               stars: 5,
@@ -28,11 +39,12 @@
               body: "This gem sucks.",
               author: "tim@example.org",
               createdOn: 1397490980837
-            }]
+            }],
+            selected: false
         },
         {
             name: 'Tome 2 : La Pierre des Larmes',
-            price: 25,
+            price: 8,
             image: "images/2_stoneoftears.jpg",
             reviews: [{
               stars: 5,
@@ -44,11 +56,12 @@
               body: "This gem sucks.",
               author: "tim@example.org",
               createdOn: 1397490980837
-            }]
+            }],
+            selected: false
         },
         {
             name: 'Tome 3 : Le Sang de la Déchirure',
-            price: 25,
+            price: 8,
             image: "images/3_bloodofthefold.jpg",
             reviews: [{
               stars: 5,
@@ -60,11 +73,12 @@
               body: "This gem sucks.",
               author: "tim@example.org",
               createdOn: 1397490980837
-            }]
+            }],
+            selected: false
         },
         {
             name: 'Tome 4 : Le Temple des Vents',
-            price: 25,
+            price: 8,
             image: "images/4_templeofthewinds.jpg",
             reviews: [{
               stars: 5,
@@ -76,11 +90,12 @@
               body: "This gem sucks.",
               author: "tim@example.org",
               createdOn: 1397490980837
-            }]
+            }],
+            selected: false
         },
         {
             name: "Tome 5 : L'Âme du Feu",
-            price: 25,
+            price: 8,
             image: "images/5_soulofthefire.jpg",
             reviews: [{
               stars: 5,
@@ -92,11 +107,12 @@
               body: "This gem sucks.",
               author: "tim@example.org",
               createdOn: 1397490980837
-            }]
+            }],
+            selected: false
         },
         {
             name: 'Tome 6 : La Foi des Réprouvés',
-            price: 25,
+            price: 8,
             image: "images/6_faithofthefallen.jpg",
             reviews: [{
               stars: 5,
@@ -108,11 +124,12 @@
               body: "This gem sucks.",
               author: "tim@example.org",
               createdOn: 1397490980837
-            }]
+            }],
+            selected: false
         },
         {
             name: 'Tome 7 : Les Piliers de la Création',
-            price: 25,
+            price: 8,
             image: "images/7_thepillarsofcreation.jpg",
             reviews: [{
               stars: 5,
@@ -124,12 +141,13 @@
               body: "This gem sucks.",
               author: "tim@example.org",
               createdOn: 1397490980837
-            }]
+            }],
+            selected: false
         },
         {
             name: "Tome 8 : L'Empire des Vaincus",
-            price: 25,
-            image: "images/8_nakedempire",
+            price: 8,
+            image: "images/8_nakedempire.jpg",
             reviews: [{
               stars: 5,
               body: "I love this gem!",
@@ -140,11 +158,12 @@
               body: "This gem sucks.",
               author: "tim@example.org",
               createdOn: 1397490980837
-            }]
+            }],
+            selected: false
         },
         {
             name: 'Tome 9 : La Chaîne des Flammes',
-            price: 25,
+            price: 8,
             image: "images/9_chainfire.jpg",
             reviews: [{
               stars: 5,
@@ -156,11 +175,12 @@
               body: "This gem sucks.",
               author: "tim@example.org",
               createdOn: 1397490980837
-            }]
+            }],
+            selected: false
         },
         {
             name: 'Tome 10 : Le Fantôme du Souvenir',
-            price: 25,
+            price: 8,
             image: "images/10_phantom.jpg",
             reviews: [{
               stars: 5,
@@ -172,11 +192,12 @@
               body: "This gem sucks.",
               author: "tim@example.org",
               createdOn: 1397490980837
-            }]
+            }],
+            selected: false
         },
         {
             name: "Tome 11 : L'Ombre d'une Inquisitrice",
-            price: 25,
+            price: 8,
             image: "images/11_confessor.jpg",
             reviews: [{
               stars: 5,
@@ -188,11 +209,12 @@
               body: "This gem sucks.",
               author: "tim@example.org",
               createdOn: 1397490980837
-            }]
+            }],
+            selected: false
         },
         {
             name: 'Tome 12 : La Machine à Présages',
-            price: 25,
+            price: 8,
             image: "images/12_theomenmachine.jpg",
             reviews: [{
               stars: 5,
@@ -204,11 +226,12 @@
               body: "This gem sucks.",
               author: "tim@example.org",
               createdOn: 1397490980837
-            }]
-        }
+            }],
+            selected: false
+        },
         {
             name: 'Tome 13 : Le Troisième Royaume',
-            price: 25,
+            price: 8,
             image: "images/13_thirdkingdom.jpg",
             reviews: [{
               stars: 5,
@@ -220,11 +243,12 @@
               body: "This gem sucks.",
               author: "tim@example.org",
               createdOn: 1397490980837
-            }]
-        }
- {
+            }],
+            selected: false
+        },
+        {
             name: 'Tome 14 : Le Crépuscule des Prophéties',
-            price: 25,
+            price: 8,
             image: "images/14_severedsouls.jpg",
             reviews: [{
               stars: 5,
@@ -236,11 +260,12 @@
               body: "This gem sucks.",
               author: "tim@example.org",
               createdOn: 1397490980837
-            }]
-        }
- {
+            }],
+            selected: false
+        },
+        {
             name: 'Tome 15 : Le Coeur de la Guerre',
-            price: 25,
+            price: 8,
             image: "images/15_warheart.jpg",
             reviews: [{
               stars: 5,
@@ -252,7 +277,39 @@
               body: "This gem sucks.",
               author: "tim@example.org",
               createdOn: 1397490980837
-            }]
+            }],
+            selected: false
         }
     ];
+
+    function CartForm() {
+        this.invoice = {
+            items: [{
+                qty: 10,
+                description: 'item',
+                cost: 9.95}]
+        };
+
+        this.addItem = function() {
+            this.invoice.items.push({
+                qty: 1,
+                description: '',
+                cost: 0
+            });
+        },
+
+        this.removeItem = function(index) {
+            this.invoice.items.splice(index, 1);
+        },
+
+        this.total = function() {
+            var total = 0;
+            angular.forEach(this.invoice.items, function(item) {
+                total += item.qty * item.cost;
+            })
+
+            return total;
+        }
+    }
+
 })();
